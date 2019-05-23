d41939582ee7605207d653f9a7bbdf5d2f5d07c1
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-23 16:06:33 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelector('.ranked-list')
+}
+
+function deepestChild() {
+  return document.querySelector('')
+}
