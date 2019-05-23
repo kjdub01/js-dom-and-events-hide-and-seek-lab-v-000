118aa1936bbc81fcb1de4566a6a32d7db2a07788
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-23 16:54:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll( '.ranked-list' );
 
   for ( let i = 0, l = rankedLists.length; i < l; i++ ) {
-    let children = rankedLists[ i ].children
+    let children = rankedLists[ i ].children;
+    
+    for ( let j = 0;)
   }
 }
 
